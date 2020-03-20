6686fbd97037d6b1cd6a5513787385e59f12efc9
Hugo Rodrigues Gomes da Silva <hugo.ifce@gmail.com>
2020-03-20 01:20:14 -0300

    Add play music function

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -7,18 +7,49 @@ import { Component } from '@angular/core';
 })
 export class HomeComponent {
 
+  public currentSong = {
+    percentage: 0,
+    seconds: 0,
+    currentTime: '00:00',
+    currentLength: '00:00',
+    currentSong: undefined
+  };
+
   public songs = [{
     title: 'I am the Highway',
     artist: 'Audioslave',
-    length: '180'
+    length: 180
   }, {
     title: 'One',
     artist: 'Metallica',
-    length: '230'
+    length: 230
   }, {
     title: 'The Unforgiven',
     artist: 'Metallica',
-    length: '130'
+    length: 130
   }];
 
+  constructor() {
+
+  }
+
+  public playSong(song) {
+    this.currentSong.percentage = 0;
+    this.currentSong.seconds = 0;
+    this.currentSong.currentSong = song;
+    this.currentSong.currentLength = this.secondsToText(song.length);
+    setInterval(() => this.updateProgress(), 5);
+  }
+
+  private updateProgress() {
+    this.currentSong.seconds += 0.005;
+    this.currentSong.currentTime = this.secondsToText(Math.floor(this.currentSong.seconds));
+    this.currentSong.percentage = (this.currentSong.seconds / this.currentSong.currentSong.length) * 100;
+  }
+
+  private secondsToText(value) {
+    const minutes: number = Math.floor(value / 60);
+    const seconds = (value - minutes * 60).toString();
+    return `${minutes}:${(seconds.length === 1) ? '0' + seconds : seconds}`;
+  }
 }
