26dadacca46247480dc4ed2e480a8ed96934960a
Hugo Rodrigues Gomes da Silva <hugo.ifce@gmail.com>
2020-03-20 00:57:00 -0300

    Add songs list

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -7,4 +7,18 @@ import { Component } from '@angular/core';
 })
 export class HomeComponent {
 
+  public songs = [{
+    title: 'I am the Highway',
+    artist: 'Audioslave',
+    length: '180'
+  }, {
+    title: 'One',
+    artist: 'Metallica',
+    length: '230'
+  }, {
+    title: 'The Unforgiven',
+    artist: 'Metallica',
+    length: '130'
+  }];
+
 }
